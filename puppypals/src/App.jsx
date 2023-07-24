1d25f5a15f118b679507195486fc08fb9c6ddd1f
jellio26 <jasminelelliott@icloud.com>
2023-07-24 15:15:10 -0400

    render the puppy

--- puppypals/src/App.jsx
+++ puppypals/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from './data'
+
+
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
-
+console.log(featuredPup)
+  console.log("puppyList: ", puppyList);
+  
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+    
+      {puppies.map((puppy) => {
+        return (
+          <p onClick= {()=>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
-
 export default App
